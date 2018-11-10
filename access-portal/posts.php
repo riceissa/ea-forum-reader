98b32f5600e4fcffd47de976f0acef7b8d082cea
Issa Rice <riceissa@gmail.com>
2018-11-09 22:35:02 -0800

    try ob start/flush

--- access-portal/posts.php
+++ access-portal/posts.php
@@ -3,8 +3,11 @@
 if ($_REQUEST['id'] ?? '') {
   $post_id = $_REQUEST['id'];
   $post_id = preg_replace('/[^a-zA-Z0-9_-]/', '', $post_id);
+  ob_start();
 
   echo shell_exec("../scrape.py " . escapeshellarg($post_id));
+
+  ob_end_flush();
 } else {
   echo 'Please enter a post ID as the "id" parameter in the URL.';
 }

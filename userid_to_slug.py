d84e6e73669dfa960740d748378aa51bb4d28838
Issa Rice <riceissa@gmail.com>
2018-11-10 14:36:10 -0800

    add comment

--- userid_to_slug.py
+++ userid_to_slug.py
@@ -3,4 +3,8 @@
 import sys
 from scrape import userid_to_userslug
 
+# For some reason, the Algolia search result JSON only has the user ID and
+# username, not the user slug.  So to be able to link to the user page from
+# search results, we need to conver the userid to a user slug.
+
 print(userid_to_userslug(sys.argv[1]))

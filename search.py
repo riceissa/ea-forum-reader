024196572062726b985fbb4ed3a0789a6bad46d8
Issa Rice <riceissa@gmail.com>
2018-11-10 14:41:25 -0800

    add

---
+++ search.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+import requests
+from urllib.parse import quote
+
+ALGOLIA_URL = ""
+with open("algolia_url.txt", "r") as f:
+    ALGOLIA_URL = next(f).strip()
+
+
+def search_posts(string):
+    data = '''{"requests":[{"indexName":"test_posts","params":"query=%s&hitsPerPage=30&page=0"}]}''' % quote(string)
+    r = requests.post(ALGOLIA_URL, data=data)
+
+    return r.json()['results'][0]['hits']
+
+
+def search_comments(string):
+    data = '''{"requests":[{"indexName":"test_comments","params":"query=%s&hitsPerPage=30&page=0"}]}''' % quote(string)
+    r = requests.post(ALGOLIA_URL, data=data)
+
+    return r.json()['results'][0]['hits']
+
+
+def show_comment(comment):
+    result = ('''<div style="border: 1px solid black;">Comment by <a href="./users.php?userid=%s">%s</a>
+        on <a href="./posts.php?id=%s">%s</a>
+        ''' % (comment['userId'], comment['authorUserName'], comment['postId'], comment['postTitle']))
+
+    result += '''<pre style="font-family: Helvetica, sans-serif; word-wrap: break-word; white-space: pre-wrap; white-space: -moz-pre-wrap;">%s</pre>\n''' % comment['body']
+    result += "</div>"
+
+    return result
+
+
+def show_post(post):
+    result = ('''<div style="border: 1px solid black;"><a href="./posts.php?id=%s">%s</a>
+    ''' % (post['_id'], post['title']))
+    result += '''<pre style="font-family: Helvetica, sans-serif; word-wrap: break-word; white-space: pre-wrap; white-space: -moz-pre-wrap;">%s</pre>\n''' % post['body']
+
+    result += "</div>\n"
+    return result
+
+def print_comments(string):
+    for comment in search_comments(string):
+        print(show_comment(comment))
+
+def print_posts(string):
+    for post in search_posts(string):
+        print(show_post(post))
+
+
+# print(json.dumps(search_posts("carl shulman"), indent=4))
+
+
+if len(sys.argv) > 1:
+    search_string = sys.argv[1]
+    print_posts(search_string)
+    print_comments(search_string)

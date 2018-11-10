be4ed517a2857f970910f2b99a13bc8499c5e170
Issa Rice <riceissa@gmail.com>
2018-11-10 01:37:31 -0800

    fix

--- access-portal/users.php
+++ access-portal/users.php
@@ -2,16 +2,19 @@
 
 if (($_REQUEST['id'] ?? '') && ($_REQUEST['format'] ?? '') === "rss") {
   $username = $_REQUEST['id'];
-  $username = preg_replace('/[^a-zA-Z0-9_-]/', '', $post_id);
+  $username = preg_replace('/[^a-zA-Z0-9_-]/', '', $username);
 
   // For some reason when Python is invoked through PHP, it runs into Unicode
   // encoding issues when trying to print (because it defaults to some
   // ASCII-only encoding). So we have to force it to use UTF-8 here.
-  $command = "PYTHONIOENCODING=utf-8 ../userfeed.py " . escapeshellarg($post_id);
+  $command = "PYTHONIOENCODING=utf-8 ../userfeed.py " . escapeshellarg($username);
 
   $output = shell_exec($command);
 
   echo $output;
 } else {
-  echo 'Please enter a post ID as the "id" parameter in the URL.';
+  echo "<pre>";
+  echo 'Please enter a username as the "id" parameter in the URL, and set format=rss.' . "\n";
+  echo "For example, /users.php?id=vipulnaik&format=rss\n";
+  echo "</pre>";
 }

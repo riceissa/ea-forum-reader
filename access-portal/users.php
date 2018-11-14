ed0370c24974b7d9c813ac166142a1e4f29e2379
Issa Rice <riceissa@gmail.com>
2018-11-13 19:25:49 -0800

    escape shell arg

--- access-portal/users.php
+++ access-portal/users.php
@@ -24,7 +24,7 @@ if ($_REQUEST['id'] ?? '') {
   // For some reason when Python is invoked through PHP, it runs into Unicode
   // encoding issues when trying to print (because it defaults to some
   // ASCII-only encoding). So we have to force it to use UTF-8 here.
-  $command = "PYTHONIOENCODING=utf-8 ../users.py " . escapeshellarg($username) . " " . $format;
+  $command = "PYTHONIOENCODING=utf-8 ../users.py " . escapeshellarg($username) . " " . escapeshellarg($format);
 
   $output = shell_exec($command);
 

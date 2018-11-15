c391ef4c4d10673892bc62ae7d439b732dbae766
Issa Rice <riceissa@gmail.com>
2018-11-15 14:54:34 -0800

    get url right

--- config_sample.py
+++ config_sample.py
@@ -5,7 +5,7 @@ PATH_STYLE = "localhost"
 # PATH_STYLE = "official"
 
 GRAPHQL_URL = 'https://forum.effectivealtruism.org/graphql'
-# GRAPHQL_URL = 'https://lesswrong/graphql'
+# GRAPHQL_URL = 'https://www.lesswrong.com/graphql'
 
 LINK_COLOR = "#326492"
 # LINK_COLOR = "#6a8a6b"

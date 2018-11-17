96be72556f0adab064d0f1be4b513145d44a12fe
Issa Rice <riceissa@gmail.com>
2018-11-17 15:38:25 -0800

    reorder

--- config_sample.py
+++ config_sample.py
@@ -5,13 +5,11 @@ PATH_STYLE = "localhost"
 # PATH_STYLE = "official"
 
 GRAPHQL_URL = 'https://forum.effectivealtruism.org/graphql'
-# GRAPHQL_URL = 'https://www.lesswrong.com/graphql'
-
 LINK_COLOR = "#326492"
-# LINK_COLOR = "#6a8a6b"
-
 COMMENT_COLOR = "#ECF5FF"
-# COMMENT_COLOR = "#F7F7F8"
-
 TITLE = "EA Forum Reader"
+
+# GRAPHQL_URL = 'https://www.lesswrong.com/graphql'
+# LINK_COLOR = "#6a8a6b"
+# COMMENT_COLOR = "#F7F7F8"
 # TITLE = "LessWrong 2.0 Reader"

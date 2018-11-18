315b39982a970dc36880ded00e5fa9b01df987d5
Issa Rice <riceissa@gmail.com>
2018-11-17 16:54:43 -0800

    use if main

--- users.py
+++ users.py
@@ -3,10 +3,11 @@
 import sys
 from scrape import *
 
-if len(sys.argv) != 2 + 1:
-    print("Unexpected number of arguments")
-else:
-    if sys.argv[2] == "rss":
-        print(feed_for_user(sys.argv[1]))
+if __name__ == "__main__":
+    if len(sys.argv) != 2 + 1:
+        print("Unexpected number of arguments")
     else:
-        print(html_page_for_user(username=sys.argv[1], display_format=sys.argv[2]))
+        if sys.argv[2] == "rss":
+            print(feed_for_user(sys.argv[1]))
+        else:
+            print(html_page_for_user(username=sys.argv[1], display_format=sys.argv[2]))

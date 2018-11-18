a86396a439e4d08771db914c7b4921ea2fe51f0f
Issa Rice <riceissa@gmail.com>
2018-11-17 16:56:30 -0800

    use if main

--- index.py
+++ index.py
@@ -3,9 +3,10 @@
 import sys
 from scrape import *
 
-if len(sys.argv) != 5+1:
-    print("Unexpected number of arguments")
-else:
-    print(show_daily_posts(offset=int(sys.argv[1]), view=sys.argv[2],
-                           before=sys.argv[3], after=sys.argv[4],
-                           display_format=sys.argv[5]))
+if __name__ == "__main__":
+    if len(sys.argv) != 5+1:
+        print("Unexpected number of arguments")
+    else:
+        print(show_daily_posts(offset=int(sys.argv[1]), view=sys.argv[2],
+                               before=sys.argv[3], after=sys.argv[4],
+                               display_format=sys.argv[5]))

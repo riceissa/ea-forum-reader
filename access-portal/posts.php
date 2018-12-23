6969d41271679a9d3e2d263eced59125b060642d
Issa Rice <riceissa@gmail.com>
2018-12-23 14:38:43 -0800

    remove test
    this didn't end up working

--- access-portal/posts.php
+++ access-portal/posts.php
@@ -13,9 +13,6 @@ if ($_REQUEST['id'] ?? '') {
   } else {
     $format = "html";
   }
-
-  echo "\n\n\n\n\n\n\n";
-
   // For some reason when Python is invoked through PHP, it runs into Unicode
   // encoding issues when trying to print (because it defaults to some
   // ASCII-only encoding). So we have to force it to use UTF-8 here.

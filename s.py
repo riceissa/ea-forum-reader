1e1b3f3b41ffe7c280b98b484e9d965832f4d8f8
Issa Rice <riceissa@gmail.com>
2020-03-13 23:31:20 -0700

    get sequence/chapters

--- s.py
+++ s.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import pdb
 
 import sys
 from urllib.parse import quote
@@ -8,7 +9,7 @@ import config
 import util
 import linkpath
 
-def get_sequence(sid, run_query=True):
+def get_sequence(sequenceid, run_query=True):
     query = ("""
     {
       sequence(
@@ -52,7 +53,7 @@ def get_sequence(sid, run_query=True):
         }
       }
     }
-    """ % sid)
+    """ % sequenceid)
 
     if not run_query:
         return query + ('''\n<a href="%s">Run this query</a>\n\n''' % (config.GRAPHQL_URL.replace("graphql", "graphiql") + "?query=" + quote(query)))
@@ -86,3 +87,17 @@ def get_chapter(chapterid, run_query=True):
 
     request = util.send_query(query)
     return util.safe_get(request.json(), ['data', 'chapter', 'result'])
+
+
+def show_sequence(sequenceid, display_format):
+    print("""<!DOCTYPE html>
+    <html>
+    """)
+    run_query = False if display_format == "queries" else True
+    sequence = get_sequence(sequenceid)
+    for chapterdict in util.safe_get(sequence, "chapters"):
+        chapterid = chapterdict["_id"]
+        chapter = get_chapter(chapterid)
+        print("Chapter:", util.safe_get(chapterdict, "title"))
+        for postdict in util.safe_get(chapter, "posts"):
+            print("  post:", util.safe_get(postdict, "title"))

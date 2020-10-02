a13c613f34bce359b3250216ea30eccc537edc06
Issa Rice <riceissa@gmail.com>
2020-10-02 01:03:38 -0700

    Use new users list view
    I was getting "502 Bad Gateway" with the existing view.

--- userlist.py
+++ userlist.py
@@ -76,7 +76,7 @@ def users_list_query(sort_by="karma", run_query=True):
         {
           users(input: {
             terms: {
-              view: "LWUsersAdmin"
+              view: "allUsers"
               limit: 500
               %s
             }
